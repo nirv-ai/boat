3e1c7c8063157ce6c3d9ef4c4ead880b516fa307
poop <noahedwardhall@gmail.com>
2023-03-12 22:53:42 -0700

    happy path: load manifest from file

--- src/boat/private/Config.nim
+++ src/boat/private/Config.nim
@@ -22,34 +22,35 @@ import std/[
   strutils,
 ]
 
-
 type Config* = ref object of RootObj
-  # pub #
   cachedir*: string ## \
     ## defaults to getCacheDir()
   use*: string ## \
     ## path on disk pointing to a manifest / or dir with a manifest
     ## URL pointing to a *.ini file
-
-  # priv #
+  #--- private ---#
   parsed: parsecfg.Config ## \
     ## the parsed config after loading
   saved: bool ## \
     ## true if self.use has been saved to disk
 
+proc parseLocalManifest*(self: Config, path = self.use): bool =
+  ## parse self.use to self.parsed
+  ## prefer calling self.load or self.reload for validation
+  self.parsed = loadConfig path
+  result = true
+
+
 proc save*(self: Config): bool =
   ## serialize Self.parsed to disk @ self.cachedir | getCachDir() / <SELF.ID>.manifest.nim.ini
   result = true
 
-proc parse*(self: Config): bool =
-  ## parse self.use to self.parsed
-  result = true
 
 proc reload*(self: Config): bool =
   # starts with https?
     # ends in .ini? break
     # throw: urls must point to a manifest.nim.ini
-  # must be a filepath, as we only support https that point to an *.ini
+  # must be a filepath, as we only support https
     # do we have read access?
       # does it end in .ini ? break
       # must be a dir
@@ -63,17 +64,19 @@ proc reload*(self: Config): bool =
     of true: raise newException(CatchableError, "TODO: remote manifests not setup")
     else:
       try:
-        let file = self.use.getFileInfo
-        case file.kind
-          of pcFile:
-            if fpUserRead notin file.permissions:
+        # TODO: test this throws if its not a file
+        # we only check if its not https, whatif ftp? http? etc
+        let path = self.use.getFileInfo
+        case path.kind
+          of pcFile, pcLinkToFile:
+            if fpUserRead notin path.permissions:
               raise newException(CatchableError, "invalid file permissions")
             elif not self.use.endsWith ".ini":
               raise newException(CatchableError, "invalid file type")
 
-            self.parsed = loadConfig self.use
-            result = true
-          else: raise newException(CatchableError, "TODO: loading from dir not setup")
+            result = self.parseLocalManifest()
+          of pcDir, pcLinkToDir:
+            raise newException(CatchableError, "TODO: loading from dir not setup")
       except:
         debugEcho repr getCurrentException()
         raise newException(CatchableError, "unable to load conf from disk")
@@ -82,6 +85,6 @@ proc reload*(self: Config): bool =
 proc load*(self: Config): bool =
   ## load whatever self.use points to
   result = if self.saved:
-    if self.parsed is Config: true # TODO: ensure this checks its not an empty Config
-    else: self.parse()
+    if self.parsed is Config: true # TODO: ensure this checks its a Config instance
+    else: true # self.parse() TODO: need to load from cacheDir
   else: self.reload()
--- tests/config/tconfig.nim
+++ tests/config/tconfig.nim
@@ -6,8 +6,15 @@ valgrind: true
 import ../bdd
 import ../helpers
 
-block baseCase:
-  let it = bdd("base case: ")
+block baseCaseLoadFile:
+  let it = bdd("base case: Load Manifest from File -> ")
   it should, "load config", newConf().load()
-  it should, "parse config", newConf().parse()
+
+block baseCaseLoadFileFromDir:
+  let it = bdd("base case: Load Manifest from Directory -> ")
+  it should, "load config", newConf().load()
+
+block todoCases:
+  let it = bdd("todo cases: ")
+  it should, "parse config", newConf().parseLocalManifest()
   it should, "save config", newConf().save()
