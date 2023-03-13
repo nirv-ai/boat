4c54f7ef28f9d28542a7419855225d67e76c76ba
poop <noahedwardhall@gmail.com>
2023-03-13 10:49:19 -0700

    add should and should not error to bdd

--- tests/bdd.nim
+++ tests/bdd.nim
@@ -1,5 +1,7 @@
 import std/sugar
 
+export sugar
+
 # TODO: both itShould & Not should be templates
 # ^ so we can lazy eval cond and wrap it in try, except
 # ^ or add an overload to accept a callback, think thats what everyone else does
@@ -16,13 +18,21 @@ proc itShouldNot*(
   cond: bool
   ): void = itShould msg, name, cond, false
 
+# TODO: follow the leader: https://www.chaijs.com/api/bdd/
 type What* = enum
-  should, shouldNot
+  should, shouldNot, shouldError, shouldNotError
 
-# TODO: find time to enhance this
-proc bdd*(caseName: string): (What, string, bool) -> void =
-  (what: What, msg: string, cond: bool) => (
+proc bdd*(caseName: string): (What, string, () -> bool) -> void =
+  (what: What, msg: string, cond: () -> bool) => (
     case what
-      of should: itShould(msg, caseName, cond)
-      of shouldNot: itShouldNot(msg, caseName, cond)
+      of should: itShould(msg, caseName, cond())
+      of shouldNot: itShouldNot(msg, caseName, cond())
+      of shouldError, shouldNotError:
+        var didError = false
+        try: discard cond()
+        except CatchableError: didError = true
+        finally:
+          if ord(what) == ord(shouldError): itShould(msg, caseName, didError)
+          else: itShouldNot(msg, caseName, didError)
+
   )
--- tests/config/tconfig.nim
+++ tests/config/tconfig.nim
@@ -1,6 +1,6 @@
 discard """
 action: "run"
-# valgrind: true
+valgrind: true
 exitcode: 0
 """
 
@@ -8,15 +8,14 @@ import ../bdd
 import ../helpers
 
 block baseCaseLoadFile:
-  doAssert true == false
   let it = bdd "base case: Load Manifest from File -> "
-  it should, "load config", newConf().load()
+  it should, "load config", () => newConf().load()
 
 block baseCaseLoadFileFromDir:
   let it = bdd "base case: Load Manifest from Directory -> "
-  it should, "load config", newConfD().load()
+  it shouldError, "load config", () => newConfD().load()
 
 block todoCases:
   let it = bdd "todo cases: "
-  it should, "parse config", newConf().parseLocalManifest()
-  it should, "save config", newConf().save()
+  it should, "parse config", () => newConf().parseLocalManifest()
+  it should, "save config", () => newConf().save()
